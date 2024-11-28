aac7f1f31f2e5baf4fac093408d53c6e2aa62a97
BuildTools <unconfigured@null.spigotmc.org>
2024-11-29 00:03:50 +0200

    Fix port binding issue

--- Backend/server.mjs
+++ Backend/server.mjs
@@ -13,34 +13,6 @@
 
    // Serve static files from the "public" directory
    app.use(express.static(path.join(__dirname, '../Frontend')));
-   // Handle POST requests to send data to Discord
-   app.post('/send-to-discord', (req, res) => {
-       const { name, email, message } = req.body;
-       const webhookURL = 'https://discord.com/api/webhooks/1311794677564837908/X-zteBu1CyS6UjDqsbBFfjPpjCYayVTHoL0hS_fF1tOzRJLPXkWT3hY2nTcVphcqd1gP';
-
-       const payload = {
-           content: `**New Contact Form Submission**\n**Name:** ${name}\n**Email:** ${email}\n**Message:** ${message}`
-       };
-
-       fetch(webhookURL, {
-           method: 'POST',
-           headers: {
-               'Content-Type': 'application/json'
-           },
-           body: JSON.stringify(payload)
-       })
-       .then(response => {
-           if (response.ok) {
-               res.status(200).send('Message sent successfully!');
-           } else {
-               res.status(500).send('Failed to send message.');
-           }
-       })
-       .catch(error => {
-           console.error('Error:', error);
-           res.status(500).send('Error sending message.');
-       });
-   });
 
    // Start the web server
    app.listen(PORT, () => {
@@ -49,4 +21,3 @@
 
    // Start the Discord bot
    startBot();
-   
